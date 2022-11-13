2c58757691924aa8740353251d813636e02459e2
kriz <ameachichristian10@gmail.com>
2022-11-13 04:08:22 +0100

    terraform

---
+++ Terraform/PBL/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block                     = "172.16.0.0/16"
+  enable_dns_support             = "true"
+  enable_dns_hostnames           = "true"
+  enable_classiclink             = "false"
+  enable_classiclink_dns_support = "false"
+  
+}
+
